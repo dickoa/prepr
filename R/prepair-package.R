8f2d7b2ec5e6c4ee9ec82b515ea9b1b281aeefe5
Ahmadou Dicko <mail@ahmadoudicko.com>
2020-05-10 20:44:02 +0000

    Add a Github mirror

--- R/prepair-package.R
+++ R/prepair-package.R
@@ -1,6 +1,6 @@
 #' A package for repairing broken GIS polygons
 #'
-#' It's based on the prepair Cpp library, more info at [https://github.com/tudelft3d/prepair](https://github.com/tudelft3d/prepair)
+#' It is based on the prepair Cpp library, more info at [https://github.com/tudelft3d/prepair](https://github.com/tudelft3d/prepair)
 #'
 #' @docType package
 #' @name prepair-package
